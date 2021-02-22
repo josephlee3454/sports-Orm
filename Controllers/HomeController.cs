e32321e317b8837b5fe1efef9d5d297fbd137606
Joe <joseph.lee3454@gmail.com>
2021-02-21 19:02:40 -0800

    added new repo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SportsORM.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsORM.Controllers
 {
@@ -112,8 +113,56 @@ namespace SportsORM.Controllers
 
         [HttpGet("level_2")]
         public IActionResult Level2()
-        {
+        {   //1
+            ViewBag.atlanticTeams = _context.Teams
+                .Include(item => item.CurrLeague)
+                .Where(item => item.CurrLeague.Name
+                .Contains("Atlantic Soccer Conference"))
+                .ToList();
+                //2
+            ViewBag.BostonPenguins = _context.Players
+                .Include(item => item.CurrentTeam)
+                .Where(item => item.CurrentTeam.TeamName == "Penguins" && item.CurrentTeam.Location == "Boston")
+                .ToList();
+            //3
+            ViewBag.ICBC= _context.Teams
+                .Where(item => item.LeagueId == 2)
+                .SelectMany(item => item.CurrentPlayers)
+                .ToList();
+            //4
+            ViewBag.ACAFLopez = _context.Teams
+                .Include(item => item.CurrLeague)
+                .Where(item => item.CurrLeague.Name == "American Conference of Amateur Football" && item.CurrLeague.Name.Contains("Lopez")) 
+                .ToList();
+            //5
+                ViewBag.ACAFLopez = _context.Teams
+                .Where(t => t.LeagueId == 7)
+                .SelectMany(p => p.CurrentPlayers)
+                .Where(currentPlayers => currentPlayers.LastName.Contains("Lopez"))
+                .ToList();
+
+                //6
+        
+            ViewBag.Sophia = _context.Teams
+                .Include(item => item.CurrentPlayers)
+                .Where(item => item.CurrentPlayers
+                .Any(item => item.FirstName == "Sophia"))
+                .ToList();
+
+                //7
+            ViewBag.AllLegSophia = _context.Leagues
+                .Include(item => item.Teams)
+                .Where(item => item.Teams.Any(item=> item.CurrentPlayers
+                .Any(player => player.FirstName.Contains("Sophia"))));
+            
+                //8
+            ViewBag.FindFlores = _context.Players
+                .Include(item => item.CurrentTeam)
+                .Where(item => item.LastName.Contains("Flores") 
+                && item.CurrentTeam.TeamName != "Roughriders");   
+                                                                                   
             return View();
+         
         }
 
         [HttpGet("level_3")]
